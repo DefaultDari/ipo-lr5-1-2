6f4cf28431109039d88463981bd0c3747e7b1456
Даниил Луценко <110895420+DefaultDari@users.noreply.github.com>
2024-11-13 09:49:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-def anagrams(string1,string2): #Функция для проверка на анаграммность
-    string1= string1.replace(" ","").lower() #Убираем пробелы и переводим в строчный вид
-    string2= string2.replace(" ","").lower() #Убираем пробелы и переводим в строчный вид
-    return sorted(string1) == sorted(string2) #Сравниваем уже отсортированные строки
 string1=input("Введите первую строку: ") #Ввод первой строки
 string2=input("Введите вторую строку: ") #Ввод второй строки
-if anagrams(string1,string2): #Проверка на истинность значения функции
+string1= string1.replace(" ","").lower() #Убираем пробелы и переводим в строчный вид
+string2= string2.replace(" ","").lower() #Убираем пробелы и переводим в строчный вид
+sorted1=sorted(string1)
+sorted2=sorted(string2)
+if sorted1==sorted2: #Проверка на истинность значения 
     print("Эти две строки - анаграммы") #Вывод
-else: #Если функция ложна
+else: #Если ложна
     print("Эти две строки - не анаграммы") #Вывод
